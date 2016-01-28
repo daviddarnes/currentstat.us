49ed9765202165807726ee48d4d150d5a40af0df
Benjamin Reid <benjamin@nouveller.com>
2016-01-28 22:43:55 +0000

    Get EJS to render the template

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 (function() {
 
+  var template = $('#status').html();
+  var statuses = JSON.parse($('#statuses').html());
+  var $app = $('#app');
+
+  statuses.forEach(function(status) {
+    var $status = $(ejs.render(template, { status: status }));
+    $app.append($status);
+  });
+
 })();
