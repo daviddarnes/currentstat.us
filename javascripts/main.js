2c5e051ba406d40ece03ea19b70c938dece6ebdf
Benjamin Reid <benjamin@nouveller.com>
2016-01-28 20:33:25 +0000

    Strip Vue out

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,3 @@
 (function() {
 
-  Vue.config.delimiters = ['[[', ']]'];
-
-  var status_box = {
-    template: '#status-box',
-    props: ['status']
-  };
-
-  new Vue({
-    el: '#app',
-    data: {
-      statuses: []
-    },
-    components: {
-      'status-box': status_box
-    },
-    ready: function() {
-      this.$http.get('statuses.json', function(data) {
-        this.statuses = data;
-      }).error(function(data, status, request) {
-        console.log(data, status, request);
-      });
-    }
-  });
-
 })();
